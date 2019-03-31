10319c72b6b1fdd72b99af191850c158d8611b6b
Ryan Bell <rcb0000@gmail.com>
2019-03-31 14:17:48 -0600

    num

--- core/num.hh
+++ core/num.hh
@@ -17,49 +17,12 @@ struct num : mem {
     return &cl_num.back().asn(*this);
   }
 
-  num& asn(const num& o){
-    neg = o.neg;
-    if(o.err)
-      cl_num.pb(*(o.err));
-    block = o.block;
-    return *this;
+  void clear(){
+    block.clear();
+    block.pb(0);
+    neg = false;
+    err = shift = NULL;
   }
-
-  num& operator=(const num& o){
-    asn(o);
-    return *this;
-  }
-
-  num& operator+=(const num& o){
-    // if(
-    // int i;
-    // for(i = 0; i < block.size(); ++i)
-
-    return *this;
-  }
-
-  num operator-=(const num& o){
-
-  }
-
-  num operator*=(const num& o){
-
-  }
-
-  num operator/=(const num& o){
-
-  }
-
-  num operator%=(const num& o){
-
-  }
-
-  num operator+(const num& o){ num n = *this; return n += o; }
-  num operator-(){ num n = *this; n.neg = !n.neg; return n; }
-  num operator-(const num& o){ num n = *this; return n -= o; }
-  num operator*(const num& o){ num n = *this; return n *- o; }
-  num operator/(const num& o){ num n = *this; return n /= o; }
-  num operator%(const num& o){ num n = *this; return n %= o; }
 
   bool operator==(const num& o){
     if(neg != o.neg || (shift && !o.shift) || (!shift && o.shift) ||
@@ -69,16 +32,121 @@ struct num : mem {
   bool operator!=(const num& o){ return !(*this == o); }
 
   bool operator<(const num& o){
-    // if(neg && !o.neg) return true;
-    // if(!neg &&
+    if(neg && !o.neg) return true;
+    if(!neg && o.neg) return false;
+
   }
 
   bool operator>(const num& o){ }
   bool operator<=(const num& o){ }
   bool operator>=(const num& o){ }
 
-  num pow(const num& e){
+  num& operator=(ll n){
+    clear();
+    neg = (n < 0);
+    block.pb(absl(n));
+    return *this;
+  }
 
+  num& operator=(const num& o){
+    clear();
+    neg = o.neg;
+    if(o.err)
+      cl_num.pb(*(o.err));
+    block = o.block;
+    return *this;
+  }
+
+  num& operator+=(const num& o){
+    if(neg && !o.neg){
+      if(-(*this) == o) *this = 0;
+    }
+    if(!neg && o.neg){
+
+    }
+    int i;
+    for(i = 0; i < block.size(); ++i)
+
+    return *this;
+  }
+
+  num& operator-=(const num& o){
+    return *this;
+  }
+
+  num& operator*=(const num& o){
+    return *this;
+  }
+
+  num& operator/=(const num& o){
+    return *this;
+  }
+
+  num operator%=(const num& o){
+    return *this;
+  }
+
+  num& operator<<=(const num& o){
+    return *this;
+  }
+
+  num& operator>>=(const num& o){
+    return *this;
+  }
+
+  num& operator&=(const num& o){
+    return *this;
+  }
+
+  num& operator|=(const num& o){
+    return *this;
+  }
+
+  num& operator^=(const num& o){
+    return *this;
+  }
+
+  num& operator+=(ll v){ *this += num(v); return *this; }
+  num& operator-=(ll v){ *this -= num(v); return *this; }
+  num& operator*=(ll v){ *this *= num(v); return *this; }
+  num& operator/=(ll v){ *this /= num(v); return *this; }
+  num& operator%=(ll v){ *this %= num(v); return *this; }
+  num& operator<<=(ll v){ *this <<= num(v); return *this; }
+  num& operator>>=(ll v){ *this >>= num(v); return *this; }
+  num& operator&=(ll v){ *this &= num(v); return *this; }
+  num& operator|=(ll v){ *this |= num(v); return *this; }
+  num& operator^=(ll v){ *this ^= num(v); return *this; }
+
+  num operator+(const num& o){ num n = *this; return n += o; }
+  num operator-(){ num n = *this; n.neg = !n.neg; return n; }
+  num operator-(const num& o){ num n = *this; return n -= o; }
+  num operator*(const num& o){ num n = *this; return n *- o; }
+  num operator/(const num& o){ num n = *this; return n /= o; }
+  num operator%(const num& o){ num n = *this; return n %= o; }
+  num operator<<(const num& o){ num n = *this; return n <<= o; }
+  num operator>>(const num& o){ num n = *this; return n >>= o; }
+  num operator&(const num& o){ num n = *this; return n &= o; }
+  num operator|(const num& o){ num n = *this; return n |= o; }
+  num operator^(const num& o){ num n = *this; return n ^= o; }
+
+  num operator+(ll v){ num n = *this; return n += num(v); }
+  num operator-(ll v){ num n = *this; return n -= num(v); }
+  num operator*(ll v){ num n = *this; return n *= num(v); }
+  num operator/(ll v){ num n = *this; return n /= num(v); }
+  num operator%(ll v){ num n = *this; return n %= num(v); }
+  num operator<<(ll v){ num n = *this; return n <<= num(v); }
+  num operator>>(ll v){ num n = *this; return n >>= num(v); }
+  num operator&(ll v){ num n = *this; return n &= num(v); }
+  num operator|(ll v){ num n = *this; return n |= num(v); }
+  num operator^(ll v){ num n = *this; return n ^= num(v); }
+
+  num pow(const num& e){
+    // ll r = 1;
+    // while(e){
+    //   if(e & 1) r = r * n % MOD;
+    //   n = n * n % MOD, e >>= 1;
+    // }
+    // return (int)r;
   }
 
   str _string(){
--- core/util.hh
+++ core/util.hh
@@ -4,6 +4,14 @@
 #define util_hh
 
 
+int abs(int a){ return (a < 0) ? -a : a; }
+int min(int a, int b){ return (a < b) ? a : b; }
+int max(int a, int b){ return (a > b) ? a : b; }
+
+ll absl(ll a){ return (a < 0) ? -a : a; }
+ll minl(ll a, ll b){ return (a < b) ? a : b; }
+ll maxl(ll a, ll b){ return (a > b) ? a : b; }
+
 double absd(double a){ return (a < 0) ? -a : a; }
 double mind(double a, double b){ return (a < b) ? a : b; }
 double maxd(double a, double b){ return (a > b) ? a : b; }
